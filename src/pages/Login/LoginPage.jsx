8fbdd5c31676a5ffa5527fc263001368a9611169
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-04-02 14:12:47 +0900

    refactor: 로그인 로직  리팩토링

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -4,27 +4,16 @@ import Saly from '../../assets/images/Saly.jpg';
 import Naver from '../../assets/svgs/Naver';
 import Kakao from '../../assets/svgs/Kakao';
 import Google from '../../assets/svgs/Google';
+import {
+  naverHandleLogin,
+  kakaoHandleLogin,
+  googleHandleLogin,
+} from '../../utils/socialLogin';
 
 export default function LoginPage() {
   var params = new URL(document.URL).searchParams;
   var code = params.get('code');
   var state = params.get('state');
-
-  const kakaoHandleLogin = () => {
-    window.location.href =
-      'https://kauth.kakao.com/oauth/authorize?client_id=b4b5d2ed58090488672fe944e6e4347a&redirect_uri=http://localhost:3000&response_type=code';
-  };
-
-  const naverHandleLogin = () => {
-    window.location.href =
-      'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=B1udHi5_Be_LA3ipNxIp&state=hLiDdL2uhPtsftcU&redirect_uri=http://localhost:3000';
-  };
-
-  const googleHandleLogin = () => {
-    window.location.href =
-      'https://accounts.google.com/o/oauth2/v2/auth?client_id=1047449344904-oe84fhmnv04mntj0ii6hp8futct0hp16.apps.googleusercontent.com' +
-      '&redirect_uri=http://localhost:3000&response_type=code&scope=email profile';
-  };
 
   return (
     <div className="wrapper">
