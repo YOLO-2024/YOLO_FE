a01b12fe0eb609baf8574a4e72a864b7bfd55889
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-04-29 18:19:41 +0900

    feat: 채팅방 생성 기능

--- src/pages/Chat/CreateChat.jsx
+++ src/pages/Chat/CreateChat.jsx
@@ -1,47 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Category from '../Post/Category';
 import '../../styles/pages/Post/NewPost.scss';
 import { PreviousIcon } from '../../assets/svgs/PreviousIcon';
 import { AddPhoto } from '../../assets/svgs/AddPhoto';
 import { useForm } from 'react-hook-form';
+import addChatPhoto from '../../assets/svgs/addChatPhoto.svg';
 
 export default function CreateChat() {
   const navigate = useNavigate();
-  const initialState = {
-    title: '',
-    content: '',
-    selectedCategories: [],
-  };
   const { register, handleSubmit } = useForm();
-  const [formState, setFormState] = useState(initialState);
+  const [chatImg, setChatImg] = useState('');
+  const [previewChatImg, setPreviewChatImg] = useState('');
+  const [selectedCategories, setSelectedCategories] = useState([]);
 
   const handleBack = () => {
     navigate(-1);
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+  const handleSelect = (value) => {
+    let updatedInterest = [];
+    if (selectedCategories.includes(value)) {
+      updatedInterest = selectedCategories.filter((arr) => arr !== value);
+    } else {
+      updatedInterest = [...selectedCategories, value];
+    }
+    setSelectedCategories(updatedInterest);
   };
-
-  const isValidForm = () => {
-    return (
-      formState.title &&
-      formState.content &&
-      formState.selectedCategories.length > 0
-    );
-  };
-
-  const setSelectedCategories = (categories) => {
-    setFormState({
-      ...formState,
-      selectedCategories: categories,
-    });
-  };
+  console.log(selectedCategories);
 
   /*
   const handleSubmit = (e) => {
@@ -50,64 +36,120 @@ export default function CreateChat() {
     setFormState(initialState);
   };
 */
-  const handleAddImg = () => {
-    console.log('clicked');
+  const handleChatImgChange = (e) => {
+    const newFile = e.target.files[0];
+    if (newFile && newFile.type.substr(0, 5) === 'image') {
+      setChatImg(newFile);
+    } else {
+      setChatImg('');
+      setPreviewChatImg(''); // 이미지가 아닐 경우 미리보기 이미지를 비웁니다.
+    }
+  };
+
+  useEffect(() => {
+    if (chatImg) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreviewChatImg(reader.result);
+      };
+      reader.readAsDataURL(chatImg);
+    } else {
+      setPreviewChatImg('');
+    }
+  }, [chatImg]);
+
+  const addChatHandler = async (data) => {
+    const chatFormData = new FormData();
+    if (chatImg) {
+      chatFormData.append('file', chatImg);
+    }
+    const jsonChat = JSON.stringify({
+      title: data.title,
+      content: data.content,
+    });
+
+    const blob = new Blob([jsonChat], { type: 'application/json' });
+    chatFormData.append('chatRoomCreateRequestDto', blob);
+
+    console.log(jsonChat);
   };
 
   return (
-    <>
-      <div className="onPostContainer">
-        <div className="previousIcon" onClick={handleBack}>
-          <PreviousIcon />
-        </div>
-        <input
-          type="submit"
-          value="등록"
-          className={`{onPost ${!isValidForm() ? 'disabled' : ''}}`}
-          onClick={handleSubmit}
-          style={{
-            background: isValidForm() ? ' #266ED7 6.68%' : '  #C2C2C2',
-            width: ' 60.371px',
-            height: '37px',
-            color: 'white',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: '8px',
-            border: 'none',
-          }}
-        />
-      </div>
-
-      <div className="Title-container">
-        <div className="Title-label">채팅방 제목</div>
-        <input
-          className="Title-input"
-          type="text"
-          placeholder="제목을 입력해주세요."
-          onChange={handleChange}
-          {...register('title')}
-        />
-      </div>
-      <div className="Category-container">
-        <div className="Category-label">카테고리</div>
-        <Category setSelectedCategories={setSelectedCategories} />
-      </div>
-      <input
-        name="content"
-        className="Content-input"
-        placeholder="채팅방에 대한 소개를 작성해주세요."
-        onChange={handleChange}
-        {...register('content')}
-      />
-      <div>
-        <div className="addImage-container" onClick={handleAddImg}>
-          <div className="AddPhoto">
-            <AddPhoto />
+    <div className="newChat_Wrapper">
+      <form onSubmit={handleSubmit(addChatHandler)}>
+        <div className="onPostContainer">
+          <div className="previousIcon" onClick={handleBack}>
+            <PreviousIcon />
           </div>
-          <div>채팅방 커버 이미지 추가</div>
+          <input
+            type="submit"
+            value="등록"
+            className="onPost"
+            onClick={handleSubmit}
+            style={{
+              background: '#c2c2c2',
+              width: ' 60.371px',
+              height: '37px',
+              color: 'white',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: '8px',
+              border: 'none',
+            }}
+          />
         </div>
-      </div>
-    </>
+
+        <div className="Title-container">
+          <div className="Title-label">채팅방 제목</div>
+          <input
+            className="Title-input"
+            type="text"
+            placeholder="제목을 입력해주세요."
+            {...register('title')}
+          />
+        </div>
+        <div className="Category-container">
+          <div className="Category-label">카테고리</div>
+          <Category setSelectedCategories={handleSelect} />
+        </div>
+        <textarea
+          name="content"
+          className="ChatContent-input"
+          placeholder="채팅방에 대한 소개를 작성해주세요."
+          {...register('content')}
+        />
+        <div
+          style={{
+            display: 'flex',
+            width: '100%',
+            position: 'fixed',
+            bottom: '0%',
+          }}
+        >
+          {previewChatImg && (
+            <div className="previewChatImg_container">
+              <img
+                src={previewChatImg}
+                alt="미리보기 이미지"
+                style={{ width: '100%', height: '100%', borderRadius: '10px' }}
+              />
+            </div>
+          )}
+          <label htmlFor="photoURLInput" className="addImage-container">
+            <img src={addChatPhoto} />
+            <div>채팅방 커버 이미지 추가</div>
+          </label>
+          <input
+            id="photoURLInput"
+            name="file"
+            type="file"
+            style={{ display: 'none' }}
+            {...register('file')}
+            onChange={handleChatImgChange}
+          />
+        </div>
+      </form>
+    </div>
   );
 }
