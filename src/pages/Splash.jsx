783696cb23d1811e334031bc7a92ae517e92d1c5
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-05-29 10:41:18 +0900

    Fix: Splash 화면페이지 수정

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -7,6 +7,7 @@ export default function Splash() {
   const navigate = useNavigate();
   const [isVisible, setIsVisible] = useState(true);
 
+  /*
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(false);
@@ -14,22 +15,21 @@ export default function Splash() {
 
     return () => clearTimeout(timer);
   }, []);
+*/
 
   if (!isVisible) {
     navigate('/login');
   }
 
   return (
-    <div>
-      <div className="splash_Container">
-        <div className="splash_Header">
-          <span className="splash_Text">1인 가구를</span>
-          <span className="splash_Text">위한</span>
-          <span className="splash_Text">소셜 커뮤니티</span>
-        </div>
-        <div className="splash_Logo">
-          <img src={splash_logo} alt="Splash Logo" />
-        </div>
+    <div className="splash_Container">
+      <div className="splash_Header">
+        <span className="splash_Text">1인 가구를</span>
+        <span className="splash_Text">위한</span>
+        <span className="splash_Text">소셜 커뮤니티</span>
+      </div>
+      <div className="splash_Logo">
+        <img src={splash_logo} alt="Splash Logo" />
       </div>
     </div>
   );
