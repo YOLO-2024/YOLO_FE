e11abc8f28a8e702d870669d4e6a5266f826d689
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-06-04 08:32:12 +0900

    Fix: 채팅방 api 호출 로직 수정

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_CLIENT_URL,
   timeout: 3000,
   headers: {
     'Content-Type': 'application/json',
