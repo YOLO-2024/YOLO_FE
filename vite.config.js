ce65aa1ee2973aca760cd470d84594436b17c2d5
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-06-04 07:23:19 +0900

    Fix: 채팅방 api 호출 로직 수정

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = mode === 'production' ? 'production' : 'development';
-  const VITE_ENDPOINT =
-    env === 'production'
-      ? import.meta.env.VITE_ENDPOINT
-      : 'http://localhost:3000';
+const VITE_NEW_ENDPOINT = 'http://localhost:3000';
 
-  return {
-    plugins: [react()],
-    server: {
-      host: 'http://localhost:3000',
-      port: 3000,
-      proxy: {
-        '/api': {
-          target: VITE_ENDPOINT,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: 'localhost',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: VITE_NEW_ENDPOINT,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    build: {
-      minify: false,
-    },
-  };
+  },
+  build: {
+    minify: false,
+  },
 });
